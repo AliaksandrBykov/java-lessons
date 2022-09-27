6f4dc8f777d24852941aca96ac8c19ac4e75217e
AliaksandrBykov <bikov412412@gmail.com>
2022-09-27 21:08:36 +0300

    Initial commit

---
+++ src/homework/Homework.java
@@ -0,0 +1,52 @@
+package homework;
+
+public class Homework {
+    public static void main(String[] args) {
+
+        // int dec = 637;
+        // int hex;
+        // hex = dec % 16;
+        // dec = dec / 16;
+        // hex = hex * 10 + dec % 16;
+        // hex = dec / 16;
+        // hex = hex * 10 + dec % 16;
+        // System.out.println( " dec -> hex for 637: " + hex );
+
+        // int dec = 637;
+        // 637/2 = 318, (1)
+        // 318/2 = 159, (0)
+        // 159/2 = 79,  (1)
+        // 79/2 = 39,  (1)
+        // 39/2 = 19,  (1)
+        // 19/2 = 9,  (1)
+        // 9/2 = 4,  (1)
+        // 4/2 = 2,  (0)
+        // 2/2 = 1,  (0)
+        // 1/2 = 0, (1)
+        // System.out.println(Integer.toBinaryString(dec));
+        // 10011111012^2 = 1·2^9+0·2^8+0·2^7+1·2^6+1·2^5+1·2^4+1·2^3+1·2^2+0·2^1+1·1 = 637^10
+
+//        int q = 13;
+//        int w = 15;
+//        q = q + 3;
+//        w = w + 4;
+//        System.out.println( q + " " + w );
+
+//        int aSum = 1000;
+//        int bSum = 500;
+//        int cDis = 100;
+//        int c1Dis = 100;
+//        cDis = aSum + bSum - cDis;
+//        System.out.println( cDis );
+//        System.out.println( c1Dis );
+
+       // int n = 2 ;
+       // int n2 =  2;
+        // n = n * n2;
+        // System.out.println(n);
+
+
+        }
+
+    }
+
---
+++ src/homework/Test.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class Test{
+    static int square (int n) {
+        int result = n * n;
+        return result;
+
+    }
+
+
+    public static void main(String[] args){
+        int n = square (2);
+        System.out.println ( n );
+
+
+    }
+}
