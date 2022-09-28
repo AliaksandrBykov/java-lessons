29618663b650ba0a834d1d769c29e9e055facd89
AliaksandrBykov <bikov412412@gmail.com>
2022-09-28 18:19:03 +0300

    Added homework3

---
+++ src/homework3/Hw.java
@@ -0,0 +1,30 @@
+package homework3;
+
+public class Hw {
+
+
+    public static String iStudyStr;
+
+    public static void main(String[] args) {
+        String iStudy = "I study Basic Java";
+        System.out.println(iStudy);
+        System.out.println(iStudy.charAt(17));
+        System.out.println(iStudy.toLowerCase());
+        System.out.println(iStudy.toUpperCase());
+        System.out.println(iStudy.replace('a','o'));
+        System.out.println(iStudy.substring(0 ,14));
+        System.out.println(iStudy.contains("Java"));
+
+        char g = 'g';
+        int e = g;
+        System.out.println(e);
+
+        double percent = (200 / 470.0) * 100;
+        System.out.println(percent);
+        
+
+
+
+
+    }
+}
