a3e999382b892f6519ac16f5280573e28c88e416
AliaksandrBykov <bikov412412@gmail.com>
2022-09-28 23:03:07 +0300

    Added homework32

---
+++ src/homework3/Hw2.java
@@ -0,0 +1,20 @@
+package homework3;
+
+import java.util.Scanner;
+
+public class Hw2 {
+    public static void main (String[] arges){
+
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("Введите ваше имя");
+//        String name = in.nextLine();
+//        System.out.println("Hello" + " " + name);
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите ваше имя");
+        String name = in.nextLine();
+        System.out.println("Ваше имя начинается с буквы А" + "  " + name);
+    }
+}
